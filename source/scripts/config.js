7aee3f76734a93a800e0292196b23315fef8194c
Robson Sobral <robson@oficiodesign.com>
2018-09-27 01:32:20 -0300

    Improve js config

--- source/scripts/config.js
+++ source/scripts/config.js
@@ -3,4 +3,12 @@ export default {
     domain: (window.location.hostname.indexOf('blockchain.votolegal.com.br') !== -1 ? '' : 'api.votolegal.com.br'),
     pathname: '/public-api/blockchain',
   },
+  candidates: {
+    domain: 'votolegal.com.br',
+    pathname: '/em',
+  },
+  receipts: {
+    domain: 'votolegal.com.br',
+    pathname: '/recibo',
+  },
 };
