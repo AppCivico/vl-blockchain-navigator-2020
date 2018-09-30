1fc92beeee79f1f20490d0f0ecb66f46218b65da
Robson Sobral <robson@oficiodesign.com>
2018-09-29 22:41:27 -0300

    Move VueJS global mixin definition to its own file

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,49 +1,10 @@
-/* global Vue, dayjs */
+/* global Vue */
 
-import config from './config';
-import utilities from './utilities';
 import vueGraph from './graph';
 import vueResults from './results';
+import vueMixin from './vueJsMixin';
 
-utilities.formatDate = function formatDate(date, format = 'YYYY-MM-DDTHH:mm:ss.SSSZ') {
-  return format !== 'YYYY-MM-DDTHH:mm:ss.SSSZ'
-    ? dayjs(date).format(format)
-    : date;
-};
-
-Vue.mixin({
-  filters: utilities,
-  data() {
-    return {
-      config,
-    };
-  },
-  methods: {
-    isDateValid(date) {
-      return dayjs(date).isValid();
-    },
-    totalAmount(items = []) {
-      return items.reduce((accumulator, currentValue) => accumulator + currentValue.amount, 0);
-    },
-    url(donation) {
-      const { slug = '' } = donation.candidate;
-      const decredDataDigest = donation.decred_data_digest || '';
-
-      const candidateUrl = slug
-        ? `https://${config.candidates.domain}${config.candidates.pathname}/${slug}`
-        : '#';
-
-      const receiptUrl = slug && decredDataDigest
-        ? `${candidateUrl}${config.receipts.pathname}/${decredDataDigest}`
-        : '#';
-
-      return {
-        candidate: candidateUrl,
-        receipt: receiptUrl,
-      };
-    },
-  },
-});
+Vue.mixin(vueMixin);
 
 window.$vueGraph = new Vue(vueGraph);
 window.$vueResults = new Vue(vueResults);
---
+++ source/scripts/vueJsMixin.js
@@ -0,0 +1,46 @@
+/* global dayjs */
+
+import config from './config';
+import utilities from './utilities';
+
+utilities.formatDate = function formatDate(date, format = 'YYYY-MM-DDTHH:mm:ss.SSSZ') {
+  return format !== 'YYYY-MM-DDTHH:mm:ss.SSSZ'
+    ? dayjs(date).format(format)
+    : date;
+};
+
+export default {
+  filters: utilities,
+  data() {
+    return {
+      config,
+    };
+  },
+  methods: {
+    isDateValid(date) {
+      return dayjs(date).isValid();
+    },
+    totalAmount(items = []) {
+      return items.reduce((accumulator, currentValue) => accumulator + currentValue.amount, 0);
+    },
+    url(donation) {
+      const {
+        slug = '',
+      } = donation.candidate;
+      const decredDataDigest = donation.decred_data_digest || '';
+
+      const candidateUrl = slug
+        ? `https://${config.candidates.domain}${config.candidates.pathname}/${slug}`
+        : '#';
+
+      const receiptUrl = slug && decredDataDigest
+        ? `${candidateUrl}${config.receipts.pathname}/${decredDataDigest}`
+        : '#';
+
+      return {
+        candidate: candidateUrl,
+        receipt: receiptUrl,
+      };
+    },
+  },
+};
