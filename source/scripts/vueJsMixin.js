fead886fcc90688bd8933be3840e121afa5c279c
Robson Sobral <robson@oficiodesign.com>
2018-10-01 19:00:58 -0300

    Add `getQueryString()` method

--- source/scripts/vueJsMixin.js
+++ source/scripts/vueJsMixin.js
@@ -26,6 +26,21 @@ export default {
     totalAmount(items = []) {
       return items.reduce((accumulator, currentValue) => accumulator + currentValue.amount, 0);
     },
+    getQueryString(url = window.location.href) {
+      const queryString = url.indexOf('?') !== -1
+        ? url.split('?')[1]
+        : [];
+      const params = {};
+      const queries = queryString.indexOf('&') !== -1
+        ? queryString.split('&')
+        : [queryString];
+      for (let i = 0; i < queries.length; i += 1) {
+        const element = queries[i].indexOf('=') !== -1
+          ? queries[i].split('=')
+          : [queries[i], null];
+        params[decodeURIComponent(element[0])] = decodeURIComponent(element[1] || '');
+      }
+      return params;
     },
   },
 };
